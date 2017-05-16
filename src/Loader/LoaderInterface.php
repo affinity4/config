17e7a3bf5f4c4a7b3a4fb555cf22aade2289da5e
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-05-16 15:50:06 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Loader/LoaderInterface.php
+++ src/Loader/LoaderInterface.php
@@ -24,11 +24,13 @@ interface LoaderInterface
      * and run the input method to convert it to an array.
      *
      * @param $file string Path of file to convert to PHP array
+     * @return void
      */
     public function __construct($file);
     
     /**
      * @param $content string The raw file contents
+     * @return void
      */
     public function input($content);
     
